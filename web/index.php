8658299af4bcc4aad94395783dbe4d05ab8ebb03
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-04 15:08:15 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -670,18 +670,18 @@ $B4="交易中途如網絡中斷 或 離開遊戲，有可能會影響魔法石�
                             )
                         	));
                           break;
-                        case ($m_message==N4 || $m_message== n4 $m_message=="全體攻擊" || $m_message=="全體 攻擊" || $m_message=="攻擊全體" || $m_message=="攻擊 全體"):
+                        
+                           case ($m_message==N5 || $m_message== n5 || $m_message=="強化符石" || $m_message=="強化 符石" || $m_message=="符石強化" || $m_message=="符石 強化"):
                                    $client->replyMessage(array(
                            'replyToken' => $event['replyToken'],
                             'messages' => array(
                                 array(
                                    'type' => 'text',
-                                   'text' => $N4
+                                   'text' => $N5
                                )
                             )
                         	));
-                          break; 
-                           
+                          break;
 
                            case 4:                          
                             $client->replyMessage(array(

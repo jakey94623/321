50da72909e6edaca10e38ec38b777831d1d59dfd
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-27 19:02:38 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -26,7 +26,7 @@ switch ($event['type']) {
         // 將Google表單轉成JSON資料
         $json = file_get_contents($googledataspi);
         $data = json_decode($json, true);           
-        $storetext=" "; 
+        $storetext="0"; 
         // 資料起始從feed.entry          
         foreach ($data['feed']['entry'] as $item) {
             // 將keywords欄位依,切成陣列

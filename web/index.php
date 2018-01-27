a79bb91238841effc19892637f806e31c8a7c76d
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-27 19:44:29 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -26,9 +26,9 @@ switch ($event['type']) {
         // 將Google表單轉成JSON資料
         $json = file_get_contents($googledataspi);
         $data = json_decode($json, true);           
-        $store_text; 
+        $store_text=''; 
         // 資料起始從feed.entry          
-          foreach ($data['feed']['entry'] as $item) {
+        foreach ($data['feed']['entry'] as $item) {
             // 將keywords欄位依,切成陣列
             $keywords = explode(',', $item['gsx$keywords']['$t']);
 

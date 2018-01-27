46e03ee2e1fc3738bd0d64f4261fb8e60bd54d50
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-01-27 19:33:38 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -55,26 +55,7 @@ switch ($event['type']) {
         
 
 
-        switch ($message['type']) {
-            case 'text':
-                // 回覆訊息
-                // 第一段 你要想找_(原字串)_ 讓我想想喔…
-                // 第二段 介紹你_______不錯喔
-                $client->replyMessage(array(
-                    'replyToken' => $event['replyToken'],
-                    'messages' => array(
-                        array(
-                            'type' => 'text',
-                            'text' => '你想要找'.$message['text'].' 讓我想想喔…',
-                        ),
-                        array(
-                            'type' => 'text',
-                            'text' => '介紹你 ' . $store_text . ' 不錯喔',
-                        )
-
-                    ),
-                ));               
-                break;
+       
             default:
                 error_log("Unsupporeted message type: " . $message['type']);
                 break;

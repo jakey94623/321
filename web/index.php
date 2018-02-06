d33e3ecab895902afffeab15886dd6d597ee51a1
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-06 18:44:31 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -38,7 +38,7 @@ foreach ($client->parseEvents() as $event) {
                            'messages' => array(
                              array(
                                    'type' => 'text',
-                                   'text' => $m_message . "（～￣▽￣～）（～￣▽￣～）\n"   
+                                   'text' => message.ID . $m_message . "（～￣▽￣～）（～￣▽￣～）\n"   
                                )
                             )                    
                      	));

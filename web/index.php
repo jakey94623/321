a672a8cbe2631b7e2ce45b626164256f1d341397
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-06 20:11:46 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -32,7 +32,7 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];
-                    $a=$message['id'];
+                    $a=$event['id'];
                 	if($m_message!="")
                 	{
                 		$client->replyMessage(array(

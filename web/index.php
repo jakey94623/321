8d603eeb9bfb1a289331f8156469e89df3568f69
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-08 18:51:10 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,6 +4,8 @@
 	$channel_access_token = "{d94WAvqAJBWRXZ3pmnlejuQ7S/Glp8CDK0FHSSLEWlypMdpiPerBs23gk/xsbQjT31RHVd1iq4YVMqqLbYiRRA0AnDPQohV2zFBBwMBK5JchWjB47muK5uiHL2l/JvkepuraSTviQNaPxMjKM7z/jwdB04t89/1O/w1cDnyilFU=}";
 	
 	
+		$myURL = "https://d10419103.herokuapp.com/"
+
 	$receive = json_decode(file_get_contents("php://input"));		
 	// 讀取收到的訊息內容
 	$text = $receive->events[0]->message->text;

4b6377f474900ef8c972b4f30043da5ec502eb41
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-08 18:43:28 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,7 +2,8 @@
 	$channel_id = "{1537195749}";
 	$channel_secret = "{f09490cd01d030f3bed923ab84c529cd}";
 	$channel_access_token = "{d94WAvqAJBWRXZ3pmnlejuQ7S/Glp8CDK0FHSSLEWlypMdpiPerBs23gk/xsbQjT31RHVd1iq4YVMqqLbYiRRA0AnDPQohV2zFBBwMBK5JchWjB47muK5uiHL2l/JvkepuraSTviQNaPxMjKM7z/jwdB04t89/1O/w1cDnyilFU=}";
-
+	
+	$myURL = "https://d10419103.herokuapp.com/update/"
 
 	$receive = json_decode(file_get_contents("php://input"));		
 	// 讀取收到的訊息內容

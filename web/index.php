9c976a1443aedea66ead5e48f17b0f43149de421
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-21 20:27:50 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -61,8 +61,7 @@ foreach ($client->parseEvents() as $event) {
         'altText' => "你好啊",
         'template' => array(
           'type' => 'confirm',
-          'text' => "你好",
-	'actions' => $actions
+          'text' => 'actions'
         )
       )
 			)

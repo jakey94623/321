b4b072a3d4679b6ea7c32a2de108f222fb6ff005
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-21 19:08:37 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -47,6 +47,18 @@ foreach ($client->parseEvents() as $event) {
                             )
                         )
                     	));
+				
+				
+				
+				$actions = array(
+  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("是", "ans=Y"),
+  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("否", "ans=N")
+);
+$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("問題", $actions);
+$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+$bot->replyMessage($replyToken,$msg);
+				
+				
                 	}
                     break;
                     

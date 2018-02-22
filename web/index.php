b2b1e496b9ace593218bab6ce29a8ed137f7fd17
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-22 19:18:25 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -126,7 +126,19 @@ foreach ($client->parseEvents() as $event) {
                     	));
                 	}
                     break;
-            }
+            }func InputMsgHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+    if r.Method == "GET" {
+        t, _ := template.ParseFiles("InputLineMsg.gtpl")
+        t.Execute(w, nil)
+		//v := url.Values{}
+    } else {
+        r.ParseForm()
+        fmt.Println("User ID:", r.Form["method"])
+        fmt.Println("User ID:", r.Form["uid"])
+        fmt.Println("Message:", r.Form["msg"])
+    }		
+}
             break;
         default:
             error_log("Unsupporeted event type: " . $event['type']);

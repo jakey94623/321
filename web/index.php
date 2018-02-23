199da68907604d23d09ef660e1551830c49feb14
D10419103 <32352569+D10419103@users.noreply.github.com>
2018-02-23 10:59:36 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -108,7 +108,7 @@ foreach ($client->parseEvents() as $event) {
             )
         )
                     	));	
-			}else if(date('Y-m-d h:i:sa')=="10:59:00am")
+			}else if(date('Y-m-d h:i:sa')=="2018-02-23 11:00:00am")
                 	{
                 		$client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
